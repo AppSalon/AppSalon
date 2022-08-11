eee6e3eda87d230262248de5a13a75ec5ac69b69
ChuchoRT <jesus.cas.vaz123@gmail.com>
2022-08-11 17:17:58 -0500

    Paquetes subidos

--- cypress/e2e/1-getting-started/prueva de seguridad.cy.js
+++ cypress/e2e/1-getting-started/prueva de seguridad.cy.js
@@ -3,9 +3,9 @@
 describe('Probar el agregar un nuevo servicio', () => {
     it(' Prueva de seguridad Rutas', () => {
         cy.visit('/cita'); //
-        cy.visit('/auth');
+       // cy.visit('/auth');
         cy.visit('/servicios');
         cy.visit('/admin');
-        
+        cy.visit('/servicios/actualizar?id=2');
     });
 });
